1e95275aa90001f0242216bf22b2426563fccb6f
DiegoBZ25 <diego.bz.2502@gmail.com>
2022-05-24 20:16:45 +0200

    Código refactorizado y documentado

---
+++ Examen1DAWN/GestionCafetera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1DAWN
+{
+    /// <summary>
+    /// Clase que se encarga de comprobar el funcionamiento de 
+    /// la clase Cafetera probando sus métodos
+    /// </summary>
+    /// <remarks>Esta clase se asemeja a una main program</remarks>
+    class GestionCafetera
+    {
+        static void main()
+        {
+            cafetera mi_cafetera_ejemplo = new cafetera("EspressoBarista", "Procoffee", 0.6, 7);
+
+            Console.WriteLine(mi_cafetera_ejemplo.CantidaAgua);
+            Console.WriteLine(mi_cafetera_ejemplo.consumocapsulas(5));
+            Console.WriteLine(mi_cafetera_ejemplo.TotalDeCapsulas);
+            Console.WriteLine(mi_cafetera_ejemplo.ComprobarRealizacionCafe(5));
+            Console.WriteLine(mi_cafetera_ejemplo.CantidaAgua);
+            mi_cafetera_ejemplo.llenardep(0.5);
+            Console.WriteLine(mi_cafetera_ejemplo.CantidaAgua);
+            mi_cafetera_ejemplo.reponercapsulas(3);
+            Console.WriteLine(mi_cafetera_ejemplo.TotalDeCapsulas);
+        }
+    }
+}
